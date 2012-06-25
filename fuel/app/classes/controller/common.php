2d3d9d885f23bf59c4056802b1c66f3c8a4e94d5
woxxy <woxxy@foolrulez.org>
2012-06-25 19:21:40 +0200

    Autologin and autologin garbage collection

---
+++ fuel/app/classes/controller/common.php
@@ -0,0 +1,36 @@
+<?php
+
+class Controller_Common extends Controller
+{
+
+	public function before()
+	{
+		$hash = FALSE;
+		if(Session::get('login_hash') !== null)
+		{
+			$hash = Session::get('login_hash');
+		}
+		else if (Cookie::get('autologin') !== null)
+		{
+			$hash = Cookie::get('autologin');
+		}
+
+		if($hash !== FALSE)
+		{
+			$query = DB::select('*')->from('user_autologin')
+				->where('login_hash', $hash)->and_where('expiration', '>', time())->execute();
+
+			if(count($query))
+			{
+				Auth::force_login($query[0]->user_id);
+			}
+
+		}
+
+		// login garbage collection
+		if(time() % 25 == 0)
+		{
+			DB::delete('user_autologin')->where('expiration', '<', time())->execute();
+		}
+	}
+}
