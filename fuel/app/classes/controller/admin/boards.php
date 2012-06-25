1d1e93b2967c232aa130e5e09c584b557dd3dbd7
Hung Tran <oohnoitz@gmail.com>
2012-06-25 01:22:47 -0500

    Added controller/model placeholders.

---
+++ fuel/app/classes/controller/admin/boards.php
@@ -0,0 +1,8 @@
+<?php
+
+class Controller_Admin_Boards extends Controller_Admin
+{
+
+}
+
+/* end of file boards.php */
---
+++ fuel/app/classes/model/post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Post;
+
+class Post extends \Model
+{
+
+}
+
+/* end of file post.php */
