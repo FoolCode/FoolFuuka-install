48bbbf49c629d6548d8da939d712bdcea8ffaa96
woxxy <woxxy@foolrulez.org>
2012-07-28 22:53:47 +0200

    404 if foolframe is not installed

--- fuel/app/classes/controller/common.php
+++ fuel/app/classes/controller/common.php
@@ -4,5 +4,14 @@
 class Controller_Common extends Controller
 {
 
+	public function before()
+	{
+		if (!\Config::get('foolfuuka.install.installed'))
+		{
+			throw new HttpNotFoundException;
+		}
+
+		parent::before();
+	}
 
 }
