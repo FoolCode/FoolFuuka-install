94e81c8717e383e169bb7444a54661beb431ac14
woxxy <woxxy@foolrulez.org>
2012-08-22 13:15:42 +0200

    Adding \Uri::uri_to_assoc() function

--- fuel/app/classes/extend/uri.php
+++ fuel/app/classes/extend/uri.php
@@ -26,4 +26,46 @@ class Uri extends \Fuel\Core\Uri
 
 		return parent::create($uri, $variables, $get_variables, $secure);
 	}
+	
+	
+	public static function uri_to_assoc($uri, $index = 0, $allowed = null)
+	{
+		if (is_string($uri))
+		{
+			$uri = explode('/', $uri);
+		}
+		
+		for ($i = 0; $i < $index; $i++)
+		{
+			array_shift($uri);
+		}
+		
+		// reorder the keys
+		$uri = array_values($uri);
+		$result = array();
+		
+		foreach ($uri as $key => $item)
+		{
+			if ($key % 2)
+			{
+				$result[$temp] = $item;
+			}
+			else
+			{
+				$temp = $item;
+			}
+		}
+		
+		if ($allowed !== null)
+		{
+			foreach ($allowed as $item)
+			{
+				$filtered[$item] = isset($result[$item]) ? $result[$item] : null;
+			}
+			
+			$result = $filtered;
+		}
+		
+		return $result;
+	}
 }
