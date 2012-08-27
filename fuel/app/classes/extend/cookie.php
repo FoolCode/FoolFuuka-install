68f04cc8b13d3ef7e305210c166e7c2ac515f5a7
Hung Tran <oohnoitz@gmail.com>
2012-08-27 15:34:13 -0500

    Reverted last commit.

--- fuel/app/classes/extend/cookie.php
+++ fuel/app/classes/extend/cookie.php
@@ -3,6 +3,11 @@
 class Cookie
 {
 
+	public static function get($name = null, $default = null)
+	{
+		return \Fuel\Core\Cookie::get($name, $default);
+	}
+
 	public static function set($name, $value, $expiration = null, $path = null, $domain = null, $secure = null, $http_only = null)
 	{
 		return \Fuel\Core\Cookie::set(\Config::get('foolframe.config.cookie_prefix').$name, $value, $expiration, $path, $domain, $secure, $http_only);
