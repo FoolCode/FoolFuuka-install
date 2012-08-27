a4507671f6fa3baa93c13867d0315e819aec56ff
Hung Tran <oohnoitz@gmail.com>
2012-08-27 15:23:48 -0500

    Removed redundant cookie function.

--- fuel/app/classes/extend/cookie.php
+++ fuel/app/classes/extend/cookie.php
@@ -3,11 +3,6 @@
 class Cookie
 {
 
-	public static function get($name = null, $default = null)
-	{
-		return \Fuel\Core\Cookie::get($name, $default);
-	}
-
 	public static function set($name, $value, $expiration = null, $path = null, $domain = null, $secure = null, $http_only = null)
 	{
 		return \Fuel\Core\Cookie::set(\Config::get('foolframe.config.cookie_prefix').$name, $value, $expiration, $path, $domain, $secure, $http_only);
