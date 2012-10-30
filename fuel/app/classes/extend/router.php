0618beb22206cbcba9f9907a901c8bfdd0944b75
woxxy <woxxy@foolrulez.org>
2012-10-30 01:51:59 +0100

    hook in router

--- fuel/app/classes/extend/router.php
+++ fuel/app/classes/extend/router.php
@@ -35,9 +35,29 @@ class Router extends \Fuel\Core\Router
 			{
 				$i++;
 				$temp_namespace = $namespace;
+
+				array_unshift($method_params, array_pop($temp_segments));
+
 				foreach (array_slice($temp_segments, 2) as $segment)
 				{
 					$temp_namespace .= '\\'.ucfirst($segment);
+				}
+
+				$hook = \Foolz\Plugin\Hook::forge('Fuel\Core\Router.parse_match.intercept')
+					->setParams([
+						'controller' => $temp_namespace,
+						'action' => $method_params[0],
+						'method_params' => array_slice($method_params, 1)
+					])
+					->execute();
+
+				// some plugin was listening to the route
+				if ($hook->get(null) !== null)
+				{
+					$match->controller = $hook->getParam('controller');
+					$match->action = $hook->getParam('action');
+					$match->method_params = $hook->getParam('method_params');
+					return $match;
 				}
 
 				if (class_exists($temp_namespace))
@@ -47,8 +67,6 @@ class Router extends \Fuel\Core\Router
 					$match->method_params = array_slice($method_params, 1);
 					return $match;
 				}
-
-				array_unshift($method_params, array_pop($temp_segments));
 			}
 		}
 
