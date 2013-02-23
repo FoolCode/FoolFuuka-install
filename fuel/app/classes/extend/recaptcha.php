fc89d20f69a51a077e078207e687f743075e5390
woxxy <woxxy@foolrulez.org>
2013-02-23 20:00:39 +0100

    Updating use of Preferences to Foolframe one in recaptcha extension

--- fuel/app/classes/extend/recaptcha.php
+++ fuel/app/classes/extend/recaptcha.php
@@ -1,19 +1,21 @@
 <?php
 
+use Foolz\Foolframe\Model\Preferences;
+
 class ReCaptcha extends \ReCaptcha\ReCaptcha
 {
-	
+
 	public static function _init()
 	{
 		parent::_init();
 
-		\Config::set('recaptcha.public_key',\Preferences::get('ff.auth.recaptcha_public'));
-		\Config::set('recaptcha.private_key',\Preferences::get('ff.auth.recaptcha_private'));
+		\Config::set('recaptcha.public_key', Preferences::get('ff.auth.recaptcha_public', ''));
+		\Config::set('recaptcha.private_key', Preferences::get('ff.auth.recaptcha_private', ''));
 	}
-	
-	
+
+
 	public static function available()
 	{
-		return \Preferences::get('ff.auth.recaptcha_private') && \Preferences::get('ff.auth.recaptcha_private');
+		return Preferences::get('ff.auth.recaptcha_private', false) && Preferences::get('ff.auth.recaptcha_private', false);
 	}
 }
