20901701ab95a57cb67d251bc3c7a185f3286c9f
Hung Tran <oohnoitz@gmail.com>
2013-04-16 11:46:27 -0500

    added gettext/sprintf function for localization

--- fuel/app/functions.php
+++ fuel/app/functions.php
@@ -8,6 +8,33 @@ function e($string)
 
 if (function_exists('_'))
 {
+	function _i()
+	{
+		$argc = func_num_args();
+		$args = func_get_args();
+		$args[0] = gettext($args[0]);
+
+		if ($argc <= 1)
+		{
+			return $args[0];
+		}
+
+		return call_user_func_array('sprintf', $args);
+	}
+
+	function _n()
+	{
+		$args = func_get_args();
+		$args[0] = ngettext($args[0], $args[1], $args[2]);
+
+		array_splice($args, 1, 1);
+
+		return call_user_func_array('sprintf', $args);
+	}
+
+	/**
+	 * @deprecated
+	 */
 	function __($text)
 	{
 		$text = _($text);
@@ -15,6 +42,9 @@ if (function_exists('_'))
 		return $text;
 	}
 
+	/**
+	 * @deprecated
+	 */
 	function _ngettext($msgid1, $msgid2, $n)
 	{
 		return ngettext($msgid1, $msgid2, $n);
@@ -22,12 +52,41 @@ if (function_exists('_'))
 }
 else
 {
+	function _i()
+	{
+		$argc = func_num_args();
+		$args = func_get_args();
+
+		if ($argc <= 1)
+		{
+			return $args[0];
+		}
+
+		return call_user_func_array('sprintf', $args);
+	}
+
+	function _n()
+	{
+		$args = func_get_args();
+		$args[0] = ($args[2] != 1) ? $args[1] : $args[0];
+
+		array_splice($args, 1, 1);
+
+		return call_user_func_array('sprintf', $args);
+	}
+
+	/**
+	 * @deprecated
+	 */
 	function __($text)
 	{
 		$text = str_replace('{{FOOL_NAME}}', \Config::get('foolframe.main.name'), $text);
 		return $text;
 	}
 
+	/**
+	 * @deprecated
+	 */
 	function _ngettext($msgid1, $msgid2, $n)
 	{
 		if($n !== 1)
