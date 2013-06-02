337de4f92310dea31e50daaf81429c13c69cdb0a
woxxy <woxxy@foolrulez.org>
2013-06-02 21:51:07 +0200

    For some reason the FuelPHP config can't be found so I just stuck the config in the init

--- fuel/core/classes/form.php
+++ fuel/core/classes/form.php
@@ -34,9 +34,23 @@ class Form
 	 */
 	public static function _init()
 	{
-		\Config::load('form', true);
+		//\Config::load('form', true);
 
-		static::$instance = static::forge('_default_', \Config::get('form'));
+		//static::$instance = static::forge('_default_', \Config::get('form'));
+		static::$instance = static::forge('_default_', array(
+			'prep_value'            => true,
+			'auto_id'               => true,
+			'auto_id_prefix'        => 'form_',
+			'form_method'           => 'post',
+			'form_template'         => "\n\t\t{open}\n\t\t<table>\n{fields}\n\t\t</table>\n\t\t{close}\n",
+			'fieldset_template'     => "\n\t\t<tr><td colspan=\"2\">{open}<table>\n{fields}</table></td></tr>\n\t\t{close}\n",
+			'field_template'        => "\t\t<tr>\n\t\t\t<td class=\"{error_class}\">{label}{required}</td>\n\t\t\t<td class=\"{error_class}\">{field} <span>{description}</span> {error_msg}</td>\n\t\t</tr>\n",
+			'multi_field_template'  => "\t\t<tr>\n\t\t\t<td class=\"{error_class}\">{group_label}{required}</td>\n\t\t\t<td class=\"{error_class}\">{fields}\n\t\t\t\t{field} {label}<br />\n{fields}<span>{description}</span>\t\t\t{error_msg}\n\t\t\t</td>\n\t\t</tr>\n",
+			'error_template'        => '<span>{error_msg}</span>',
+			'required_mark'         => '*',
+			'inline_errors'         => false,
+			'error_class'           => 'validation_error',
+		));
 	}
 
 	public static function forge($fieldset = 'default', array $config = array())
